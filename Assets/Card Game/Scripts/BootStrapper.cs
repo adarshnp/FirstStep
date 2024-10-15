4edc2824b00e8075adf934086fe5e7ba10e65f04
Adarsh N P <adarshnp49@gmail.com>
2024-10-15 22:27:31 +0400

    Scene Hierarchy restructured and bootstrapper added
    Scene hierarchy cleaned up and re-organized.
    Bootstrapper script added for controlling scripts execution.

---
+++ Assets/Card Game/Scripts/BootStrapper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootStrapper : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject gameLevel;
+
+    private void Start()
+    {
+        gameLevel.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
