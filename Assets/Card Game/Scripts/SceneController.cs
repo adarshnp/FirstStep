a6dfda6fb8765594884c77c29b6fcc85de136991
Adarsh N P <adarshnp49@gmail.com>
2024-10-16 21:03:15 +0400

    Added Home button to game level
    Added home button to side UI in game board.
    added functionality  to the same.
    bootstrapper class changed to SceneController. it intends to do all the functions to navigate between sections. Will change to a better name in future.

--- Assets/Card Game/Scripts/SceneController.cs
+++ Assets/Card Game/Scripts/SceneController.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// bootstrapper class that controls execution of scripts . 
+/// SceneController class that controls execution of scripts and UI order . 
 /// WIll be helpful as more scripts get added later.
 /// </summary>
-public class BootStrapper : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject gameLevel;
 
     private void Start()
     {
-        gameLevel.SetActive(false);
-        mainMenu.SetActive(true);
+        GoToMainMenu();
     }
 
     public void OpenGameScene()
     {
         mainMenu.SetActive(false);
         gameLevel.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        gameLevel.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
